8b49bd2aaccf98bb250928afd6cc206e512fc346
Mykola-Ts <125122747+Mykola-Ts@users.noreply.github.com>
2023-10-18 15:02:05 +0000

    Deploying to gh-pages from @ Mykola-Ts/goit-react-hw-08-phonebook@1ab5e8f4e1fff7bbda311702280cd759e5f76cbe 🚀

---
+++ static/js/373.14b2beaf.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[373],{4373:function(t,h,c){c.d(h,{M0G:function(){return n}});var e=c(9983);function n(t){return(0,e.w_)({tag:"svg",attr:{viewBox:"0 0 512 512"},child:[{tag:"path",attr:{d:"M304 256c52.805 0 96-43.201 96-96s-43.195-96-96-96-96 43.201-96 96 43.195 96 96 96zm0 48c-63.598 0-192 32.402-192 96v48h384v-48c0-63.598-128.402-96-192-96zM112 224v-64H80v64H16v32h64v64h32v-64h64v-32h-64z"}}]})(t)}}}]);
+//# sourceMappingURL=373.14b2beaf.chunk.js.map
